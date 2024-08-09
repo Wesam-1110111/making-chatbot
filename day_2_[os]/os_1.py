9f14c167bac9e1ed14adfdf3ebd6d0b0f3cb8a92
Wesam <mrw202065@gmail.com>
2024-08-09 19:19:22 +0300

    upload

---
+++ day_2_[os]/os_1.py
@@ -0,0 +1,31 @@
+import os
+
+print(os.path)
+
+os.system('cls') # for windows
+
+os.system('clear') # for linux
+
+os.system('dir') # for windows
+
+os.system('ls') # for linux
+
+os.system('mkdir test')
+
+os.system('cd test')
+
+print(os.path)
+
+os.system('touch main.py')
+
+os.system('ls')
+
+os.system('rm main.py')
+
+os.system('ls')
+
+os.system('cd ..')
+
+os.system('rmdir test')
+
+os.system('ls')
