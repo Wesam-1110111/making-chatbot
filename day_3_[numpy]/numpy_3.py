c3f2017e441ed5b6c5b99a29e5713be33401ee83
Wesam-1110111 <mrw202065@gmail.com>
2024-08-10 19:46:09 +0200

    done

--- day_3_[numpy]/numpy_3.py
+++ day_3_[numpy]/numpy_3.py
@@ -1,1 +1,30 @@
-numpy_3
+import numpy as np
+
+
+
+a1 = np.array((1, 2, 3, 4), dtype=int) # int or 'int', 'i'
+
+print(a1)
+print(a1.dtype)
+
+print()
+
+a2 = np.array((0, 5, 0, 77), dtype=float) # float or 'float', 'f'
+
+print(a2)
+print(a2.dtype)
+
+print()
+
+a3 = a2.astype(bool)
+
+print(a3)
+print(a3.dtype)
+
+
+print()
+
+a4 = a3.astype(str)
+
+print(a4)
+print(a4.dtype)
