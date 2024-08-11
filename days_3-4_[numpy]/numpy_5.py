94bc98ee4e0b169a574f29c28b81ed124d2aeccf
Wesam-1110111 <mrw202065@gmail.com>
2024-08-11 16:32:20 +0200

    sitll learning

--- days_3-4_[numpy]/numpy_5.py
+++ days_3-4_[numpy]/numpy_5.py
@@ -1,1 +1,18 @@
-numpy_5
+import numpy as np  
+
+
+
+
+
+arr1 = np.array([0, 1, 2, 3, 4, 5, 6, 7])
+
+print(arr1)
+print(arr1.ndim)
+print(arr1.shape)
+
+print('\n- - - - - - - - - - - - - - - - - - - - - - \nEx 2:\n')
+
+arr2 = arr1.reshape(4, 2)
+print(arr2)
+print(arr2.ndim)
+print(arr2.shape)
